5566fa3ac2f279a07658fd4ee1da66c571384e7a
mariem-mourad <134215114+mariem-mourad@users.noreply.github.com>
2023-05-22 18:35:30 +0300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImages from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+
+
+function App() {
+    return(
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                          <ProfileCard 
+                          title="Alexa"
+                           handle="@alexa99" 
+                           image={AlexaImages} 
+                           description=" Alexa was created by Amazone and helps you buy things."
+                           />
+                        </div>
+                        <div className='column is-4'>
+                          <ProfileCard 
+                            title="Cortana"
+                             handle="@cortana32" 
+                             image={CortanaImage}
+                             description= "Cortana was made by microsoft. what it dose?"
+                             /> 
+                        </div>
+                        <div className='column is-4'>
+                          <ProfileCard 
+                          title="Siri"
+                           handle="@siri01"
+                            image={SiriImage}
+                            description= "Siri was made by Apple and is being phased out"
+                            /> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
+export default App;
